43e55527b8c307e73bca9adb4b5439531be4f388
c-jinwoo <jihn011258@naver.com>
2023-10-31 12:22:05 +0900

    init

---
+++ app/src/main/java/com/swe/buddybud/Data.java
@@ -0,0 +1,6 @@
+package com.swe.buddybud;
+
+public class Data {
+    public static boolean loginRemember = false;    // Login 화면 Remember Me 체크여부
+    public static String loginEmail = "";           // Login 화면 이메일 저장용
+}
---
+++ app/src/main/java/com/swe/buddybud/LoginActivity.java
@@ -0,0 +1,85 @@
+package com.swe.buddybud;
+
+import static android.widget.Toast.makeText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    // 화면 복귀 시 박스 초기화
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextPassword = findViewById(R.id.editTextPassword);
+        CheckBox checkBoxRemember = findViewById(R.id.checkBoxRememberMe);
+
+        editTextEmail.setText(Data.loginRemember ? Data.loginEmail : "");
+        editTextPassword.setText("");
+        checkBoxRemember.setChecked(Data.loginRemember);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        Data.loginEmail = editTextEmail.getText().toString();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        CheckBox checkBoxRemember = findViewById(R.id.checkBoxRememberMe);
+        Button btnSignIn = findViewById(R.id.buttonSignIn);
+        TextView textViewSignUp = findViewById(R.id.textViewSignUp);
+
+        // Remember me 체크 박스 클릭시
+        checkBoxRemember.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Data.loginRemember = checkBoxRemember.isChecked();
+            }
+        });
+
+        // Sign In 버튼 클릭 시 : ContentActivity
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextEmail = findViewById(R.id.editTextEmail);
+                EditText editTextPassword = findViewById(R.id.editTextPassword);
+
+                if (editTextEmail.getText().length() == 0 || editTextPassword.getText().length() == 0) {
+                    makeText(getApplicationContext(), "Please fill the form",Toast.LENGTH_LONG);
+                }
+                else {
+                    Intent intent = new Intent(getApplicationContext(), ContentActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        // Sign Up 버튼 클릭시 : SignUpFstActivity
+        textViewSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), SignUpFstActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
