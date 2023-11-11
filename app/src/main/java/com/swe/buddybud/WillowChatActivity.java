8e8d4ea7fd1aab1ae19e175c767149c8a7f05156
JungHyeokAhn <yehsi2000@gmail.com>
2023-11-11 18:21:54 +0900

    feat : willow chatactivity frame

---
+++ app/src/main/java/com/swe/buddybud/WillowChatActivity.java
@@ -0,0 +1,14 @@
+package com.swe.buddybud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WillowChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_willow_chat);
+    }
+}
