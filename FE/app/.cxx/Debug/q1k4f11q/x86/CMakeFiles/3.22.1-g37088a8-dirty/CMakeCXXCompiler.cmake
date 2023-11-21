db88be650e523f3ad837e0a85bfed2ada463eec9
Your Name <you@example.com>
2023-11-21 16:04:50 +0900

    home test commit

--- FE/app/.cxx/Debug/q1k4f11q/x86/CMakeFiles/3.22.1-g37088a8-dirty/CMakeCXXCompiler.cmake
+++ FE/app/.cxx/Debug/q1k4f11q/x86/CMakeFiles/3.22.1-g37088a8-dirty/CMakeCXXCompiler.cmake
@@ -1,4 +1,4 @@
-set(CMAKE_CXX_COMPILER "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/clang++.exe")
+set(CMAKE_CXX_COMPILER "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/clang++.exe")
 set(CMAKE_CXX_COMPILER_ARG1 "")
 set(CMAKE_CXX_COMPILER_ID "Clang")
 set(CMAKE_CXX_COMPILER_VERSION "12.0.8")
@@ -22,11 +22,11 @@ set(CMAKE_CXX_SIMULATE_VERSION "")
 
 
 
-set(CMAKE_AR "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar.exe")
+set(CMAKE_AR "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ar.exe")
 set(CMAKE_CXX_COMPILER_AR "CMAKE_CXX_COMPILER_AR-NOTFOUND")
-set(CMAKE_RANLIB "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ranlib.exe")
+set(CMAKE_RANLIB "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/llvm-ranlib.exe")
 set(CMAKE_CXX_COMPILER_RANLIB "CMAKE_CXX_COMPILER_RANLIB-NOTFOUND")
-set(CMAKE_LINKER "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/ld.lld.exe")
+set(CMAKE_LINKER "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/ld.lld.exe")
 set(CMAKE_MT "")
 set(CMAKE_COMPILER_IS_GNUCXX )
 set(CMAKE_CXX_COMPILER_LOADED 1)
@@ -78,13 +78,13 @@ set(CMAKE_ANDROID_NDK_TOOLCHAIN_VERSION "clang")
 set(CMAKE_CXX_ANDROID_TOOLCHAIN_MACHINE "i686-linux-android")
 set(CMAKE_CXX_ANDROID_TOOLCHAIN_VERSION "")
 set(CMAKE_CXX_COMPILER_EXTERNAL_TOOLCHAIN "")
-set(CMAKE_CXX_ANDROID_TOOLCHAIN_PREFIX "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/i686-linux-android-")
+set(CMAKE_CXX_ANDROID_TOOLCHAIN_PREFIX "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/bin/i686-linux-android-")
 set(CMAKE_CXX_ANDROID_TOOLCHAIN_SUFFIX ".exe")
 
 
 
 
-set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/c++/v1;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/local/include;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/12.0.8/include;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/i686-linux-android;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include")
+set(CMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/c++/v1;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/local/include;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/12.0.8/include;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include/i686-linux-android;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/include")
 set(CMAKE_CXX_IMPLICIT_LINK_LIBRARIES "atomic;m;c++;m;-l:libunwind.a;dl;c;-l:libunwind.a;dl")
-set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/12.0.8/lib/linux/i386;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib/gcc/i686-linux-android/4.9.x;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/i686-linux-android/28;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/i686-linux-android;C:/Users/jihn0/AppData/Local/Android/Sdk/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib")
+set(CMAKE_CXX_IMPLICIT_LINK_DIRECTORIES "C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib64/clang/12.0.8/lib/linux/i386;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/lib/gcc/i686-linux-android/4.9.x;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/i686-linux-android/28;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib/i686-linux-android;C:/Users/YooUkcheol/AndroidSDK/ndk/23.1.7779620/toolchains/llvm/prebuilt/windows-x86_64/sysroot/usr/lib")
 set(CMAKE_CXX_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")
--- FE/app/src/main/java/com/swe/buddybud/home/DataManager.java
+++ FE/app/src/main/java/com/swe/buddybud/home/DataManager.java
@@ -16,36 +16,36 @@ public class DataManager {
         List<CommentData> comment2 = new ArrayList<>();
         List<CommentData> comment1 = new ArrayList<>();
 
-        comment3.add(new CommentData(1, R.drawable.test, "seven77", "23/11/14 17:34",
+        comment3.add(new CommentData(1, R.drawable.logo_profile, "seven77", "23/11/14 17:34",
                 "오늘 기숙사 짐 빼는 날이잖아 ㅋㅋ", "Today is the day to unpack your dorm room haha!", 7, 0, 1,null));
         comment3.add(new CommentData(2,R.drawable.profile_2, "Woosik12", "23/11/14 17:45",
                 "아 맞네 이걸 몰랐네;", "Oh right, I didn't know this;", 5, 1, 1,"seven77"));
-        comment3.add(new CommentData(3,R.drawable.test, "happy486", "23/11/14 17:46",
+        comment3.add(new CommentData(3,R.drawable.logo_profile, "happy486", "23/11/14 17:46",
                 "君はばかだよ", "You're stupid", 0, 0, 1,"Woosik12"));
         comment3.add(new CommentData(4,R.drawable.profile_2, "Woosik12", "23/11/14 17:47",
                 "ㅠㅠㅠ", "(crying)", 0, 0, 1,"happy486"));
         comment3.add(new CommentData(5,R.drawable.profile_3, "s_k_you", "23/11/14 17:35",
                 "Where can I get the delivery box?", "Where can I get the delivery box?", 0, 0, 5,null));
-        comment3.add(new CommentData(6,R.drawable.test, "유저F", "23/11/14 17:36",
+        comment3.add(new CommentData(6,R.drawable.logo_profile, "유저F", "23/11/14 17:36",
                 "多分郵便局に行かなければならないだろう", "You'll probably have to go to the post office.", 0, 0, 5,"s_k_you"));
 
         comment2.add(new CommentData(1, R.drawable.profile_1, "스꾸", "23/11/14 17:34",
                 "저 관심 있어요!", "I'm interested!", 1, 0, 1,null));
-        comment2.add(new CommentData(2,R.drawable.test, "머라하지", "23/11/14 17:45",
+        comment2.add(new CommentData(2,R.drawable.logo_profile, "머라하지", "23/11/14 17:45",
                 "저도용 근데 라켓 없어도 되나요?", "Me too, but can I do it without a racket?", 0, 2, 1,"스꾸"));
-        comment2.add(new CommentData(3,R.drawable.test, "Joy", "23/11/14 17:50",
+        comment2.add(new CommentData(3,R.drawable.logo_profile, "Joy", "23/11/14 17:50",
                 "as far as I know, Joa Tennis has racket rental service", "as far as I know, Joa Tennis has racket rental service", 5, 0, 1,"머라하지"));
-        comment2.add(new CommentData(4,R.drawable.test, "软锥", "23/11/14 17:41",
+        comment2.add(new CommentData(4,R.drawable.logo_profile, "软锥", "23/11/14 17:41",
                 "这是我第一次打网球，可以吗？", "This is my first time playing tennis, is it okay?", 1, 1, 4,null));
-        comment2.add(new CommentData(5,R.drawable.test, "milililili", "23/11/14 17:41",
+        comment2.add(new CommentData(5,R.drawable.logo_profile, "milililili", "23/11/14 17:41",
                 "当然！", "Certainly!", 2, 0, 4,"软锥"));
 
-        comment1.add(new CommentData(1,R.drawable.test, "软锥", "23/11/14 18:34",
+        comment1.add(new CommentData(1,R.drawable.logo_profile, "软锥", "23/11/14 18:34",
                 "비밀 댓글입니다", "번역 test1", 15, 2, 1,null));
 
         feedList.add(new FeedData(3, R.drawable.profile_2, "Woosik12", "23/11/14 17:18", "오늘 신관 앞에 사람 많은데",
                 "무슨 일임?","There's a crowd in front of shin-gwan", "What's going on?",  13, 6, comment3, null));
-        feedList.add(new FeedData(2, R.drawable.icon_account, "mililili", "23/11/14 17:13", "Anyone who wants to play tennis?",
+        feedList.add(new FeedData(2, R.drawable.mililili, "mililili", "23/11/14 17:13", "Anyone who wants to play tennis?",
                 "any tennis newbies around? I'm looking to hit the court around campus. \uD83D\uDE06 DM me if you're up for some smashing fun \uD83D\uDE4C",
                 "Anyone who wants to play tennis?", "any tennis newbies around? I'm looking to hit the court around campus. \uD83D\uDE06 DM me if you're up for some smashing fun \uD83D\uDE4C", 16, 5, comment2, null));
         feedList.add(new FeedData(1, R.drawable.profile_frog, "Frogy123", "23/11/14 16:33", "韓国料理のおすすめ",
