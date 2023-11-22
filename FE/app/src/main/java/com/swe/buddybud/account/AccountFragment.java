fa51316c997265e8299930347cab3970e5f10d4f
dbwowns10 <dbwowns10@g.skku.edu>
2023-11-22 16:58:30 +0900

    update code

--- FE/app/src/main/java/com/swe/buddybud/account/AccountFragment.java
+++ FE/app/src/main/java/com/swe/buddybud/account/AccountFragment.java
@@ -2,94 +2,102 @@ package com.swe.buddybud.account;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ScrollView;
 
 import com.swe.buddybud.R;
+import com.swe.buddybud.account.PostScrapAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class AccountFragment extends Fragment {
 
-    private ScrollView postsScrollView; // 게시물 ScrollView
-    private ScrollView scrapsScrollView; // 스크랩 ScrollView
-    private Button myPostsButton;
-    private Button myScrapsButton;
-    private ImageView profileImage;
+    private RecyclerView postsRecyclerView, scrapsRecyclerView;
 
     public AccountFragment() {
         // Required empty public constructor
     }
 
-    public static AccountFragment newInstance(String param1, String param2) {
-        AccountFragment fragment = new AccountFragment();
-        Bundle args = new Bundle();
-        // args.putString(ARG_PARAM1, param1);
-        // args.putString(ARG_PARAM2, param2);
-        // fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            // Handle fragment arguments if any
-        }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account, container, false);
 
-        postsScrollView = view.findViewById(R.id.posts_scroll_view);
-        scrapsScrollView = view.findViewById(R.id.scraps_scroll_view);
-        myPostsButton = view.findViewById(R.id.my_posts_button);
-        myScrapsButton = view.findViewById(R.id.my_scraps_button);
+        postsRecyclerView = view.findViewById(R.id.posts_recycler_view);
+        scrapsRecyclerView = view.findViewById(R.id.scraps_recycler_view);
 
-        // Set up the listeners for the buttons
+        // toggle button
+        Button myPostsButton = view.findViewById(R.id.my_posts_button);
+        Button myScrapsButton = view.findViewById(R.id.my_scraps_button);
+
+        // recycle view 
+        setupRecyclerView(postsRecyclerView, loadPosts());
+        setupRecyclerView(scrapsRecyclerView, loadScraps()); // Initially empty
+
+        // 유저 프로파일 이미지 설정
+        ImageView profileImageView = view.findViewById(R.id.profile_image);
+
+        // 유저 프로파일 이미지 클릭하면 계정 설정 창으로 넘어가게 만드는 클릭 리스너
+        profileImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), AccountEditActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 내 포스트 보이게 만드는 포스트 버튼 클릭 리스너
         myPostsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showPosts();
+                postsRecyclerView.setVisibility(View.VISIBLE);
+                scrapsRecyclerView.setVisibility(View.GONE);
             }
         });
 
+        // 내 스크랩 보이게 만드는 스크랩 버튼 클릭 리스너
         myScrapsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                showScraps();
-            }
-        });
-
-        profileImage = view.findViewById(R.id.profile_image);
-        profileImage.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openAccountEdit();
+                postsRecyclerView.setVisibility(View.GONE);
+                scrapsRecyclerView.setVisibility(View.VISIBLE);
             }
         });
 
         return view;
     }
-    private void showPosts() {
-        postsScrollView.setVisibility(View.VISIBLE); // 게시물 ScrollView를 보이게 설정
-        scrapsScrollView.setVisibility(View.GONE); // 스크랩 ScrollView를 숨김
+
+    private void setupRecyclerView(RecyclerView recyclerView, List<PostScrapData> data) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        PostScrapAdapter adapter = new PostScrapAdapter(data);
+        recyclerView.setAdapter(adapter);
     }
 
-    private void showScraps() {
-        postsScrollView.setVisibility(View.GONE); // 게시물 ScrollView를 숨김
-        scrapsScrollView.setVisibility(View.VISIBLE); // 스크랩 ScrollView를 보이게 설정
+    private List<PostScrapData> loadPosts() {
+        List<PostScrapData> posts = new ArrayList<>();
+
+        String userid = com.swe.buddybud.user.LoginData.getLoginUserId();
+
+        // Add test data with the userid from LoginData
+//        posts.add(new PostScrapData(userid, "2023-11-01 10:00", "Post Title 1", "Content of the first post"));
+//        posts.add(new PostScrapData(userid, "2023-11-01 11:30", "Post Title 2", "Content of the second post"));
+//        posts.add(new PostScrapData(userid, "2023-11-01 12:45", "Post Title 3", "Content of the third post"));
+        return posts;
     }
 
-    private void openAccountEdit() {
-        // 계정 정보를 수정할 액티비티로 이동
-        Intent intent = new Intent(getActivity(), AccountEditActivity.class);
-        startActivity(intent);
+    private List<PostScrapData> loadScraps() {
+        List<PostScrapData> scraps = new ArrayList<>();
+        // Add test data
+//        scraps.add(new PostScrapData("User1", "2023-11-01 10:00", "Scrap Title 1", "Content of the first scrap"));
+//        scraps.add(new PostScrapData("User2", "2023-11-01 11:30", "Scrap Title 2", "Content of the second scrap"));
+//        scraps.add(new PostScrapData("User3", "2023-11-01 12:45", "Scrap Title 3", "Content of the third scrap"));
+        return scraps;
     }
 }
