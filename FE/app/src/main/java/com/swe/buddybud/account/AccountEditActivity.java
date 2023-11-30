f19093eb85d0cb55202d2f5702d1bf5622e8137e
dbwowns10 <dbwowns10@g.skku.edu>
2023-11-30 21:11:02 +0900

    update user info edit

--- FE/app/src/main/java/com/swe/buddybud/account/AccountEditActivity.java
+++ FE/app/src/main/java/com/swe/buddybud/account/AccountEditActivity.java
@@ -1,8 +1,13 @@
 package com.swe.buddybud.account;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -14,28 +19,38 @@ import com.swe.buddybud.R;
 import com.swe.buddybud.common.RetrofitClient;
 import com.swe.buddybud.user.LoginData;
 
+import java.io.IOException;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class AccountEditActivity extends AppCompatActivity {
 
-    private TextView userIdTextview;
-    private EditText userGenderEdit;
-    private Spinner languageSpinner;
+    private EditText editUserId;
+    private Spinner genderSpinner, languageSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_edit);
 
-        userIdTextview = findViewById(R.id.user_id_edit);
+        editUserId = findViewById(R.id.editTextID);
         String userId = LoginData.getLoginUserId();
-        userIdTextview.setText(userId);
+        editUserId.setText(userId);
 
-        userGenderEdit = findViewById(R.id.user_gender_edit);
-        languageSpinner = findViewById(R.id.language_spinner);
-        Button doneButton = findViewById(R.id.done_button);
+        genderSpinner = findViewById(R.id.spinnerGender);
+        languageSpinner = findViewById(R.id.spinnerLanguage);
+
+        // Language 스피너 설정
+        String[] languageArray = getResources().getStringArray(R.array.Language);
+        setSpinner(languageSpinner, languageArray, LoginData.getLoginUserLang());
+
+        // Gender 스피너 설정
+        String[] genderArray = getResources().getStringArray(R.array.Gender);
+        setSpinner(genderSpinner, genderArray, LoginData.getLoginUserGender());
+
+        Button doneButton = findViewById(R.id.buttonDone);
 
         ImageButton goBackButton = findViewById(R.id.go_back_button);
         goBackButton.setOnClickListener(new View.OnClickListener() {
@@ -45,42 +60,87 @@ public class AccountEditActivity extends AppCompatActivity {
             }
         });
 
-        // Set up the done button to collect the data and send it to the server
         doneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Collect data from fields
-                String userId = LoginData.getLoginUserId();
+                String usernum = String.valueOf(LoginData.getLoginUserNo());
+                String userid = editUserId.getText().toString();
                 String language = languageSpinner.getSelectedItem().toString();
-                String gender = userGenderEdit.getText().toString();
+                String gender = genderSpinner.getSelectedItem().toString();
 
-                // Create an instance of UserAccountUpdate
-                UserAccountUpdate updateInfo = new UserAccountUpdate(userId, language, gender);
-
-                // Send data to server
-                updateUserAccount(userId, updateInfo);
+                UserAccountUpdate updateInfo = new UserAccountUpdate(userid, language, gender);
+                updateUserAccount(usernum, updateInfo);
             }
         });
     }
 
-    private void updateUserAccount(String userId, UserAccountUpdate updateInfo) {
+    private void updateUserAccount(String usernum, UserAccountUpdate updateInfo) {
         AccountApiService service = RetrofitClient.getService(AccountApiService.class);
-        Call<Void> call = service.updateUserAccount(userId, updateInfo);
-        call.enqueue(new Callback<Void>() {
+        Call<UpdateResponse> call = service.updateUserAccount(usernum, updateInfo);
+        call.enqueue(new Callback<UpdateResponse>() {
             @Override
-            public void onResponse(Call<Void> call, Response<Void> response) {
-                if (response.isSuccessful()) {
-                    Toast.makeText(AccountEditActivity.this, "Account updated successfully", Toast.LENGTH_SHORT).show();
-                    finish(); // Close the activity
+            public void onResponse(Call<UpdateResponse> call, Response<UpdateResponse> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    boolean updateResult = response.body().isUpdateResult();
+                    if (updateResult) {
+                        // Update was successful
+                        LoginData.setLoginUserId(updateInfo.getUserid());
+                        LoginData.setLoginUserLang(updateInfo.getLang());
+                        LoginData.setLoginUserGender(updateInfo.getGender());
+                        Toast.makeText(AccountEditActivity.this, "Account updated successfully", Toast.LENGTH_SHORT).show();
+                        finish();
+                    } else {
+                        // Update failed
+                        Toast.makeText(AccountEditActivity.this, "Failed to update account, code 2", Toast.LENGTH_SHORT).show();
+                    }
                 } else {
-                    Toast.makeText(AccountEditActivity.this, "Failed to update account", Toast.LENGTH_SHORT).show();
+                    // Response is not successful, inspect the error body
+                    if (response.errorBody() != null) {
+                        try {
+                            String errorString = response.errorBody().string();
+                            Log.e("API Error", "Error response: " + errorString);
+                            Toast.makeText(AccountEditActivity.this, "Error: " + errorString, Toast.LENGTH_LONG).show();
+                        } catch (IOException e) {
+                            Log.e("API Error", "Error parsing error response", e);
+                        }
+                    } else {
+                        Toast.makeText(AccountEditActivity.this, "Failed to update account, code 1", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
 
             @Override
-            public void onFailure(Call<Void> call, Throwable t) {
+            public void onFailure(Call<UpdateResponse> call, Throwable t) {
+//                Log.e("Network error", t.getMessage());
                 Toast.makeText(AccountEditActivity.this, "Network error", Toast.LENGTH_SHORT).show();
+            }
+
+        });
+    }
+
+
+    private void setSpinner(Spinner spinner, String[] items, String defaultValue) {
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, items);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // Set default value if not null
+        if (defaultValue != null) {
+            int spinnerPosition = adapter.getPosition(defaultValue);
+            spinner.setSelection(spinnerPosition);
+        }
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.BLACK);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
             }
         });
     }
+
+
 }
--- FE/app/src/main/java/com/swe/buddybud/account/UserAccountUpdate.java
+++ FE/app/src/main/java/com/swe/buddybud/account/UserAccountUpdate.java
@@ -1,15 +1,45 @@
 package com.swe.buddybud.account;
 
+import com.google.gson.annotations.SerializedName;
+
 public class UserAccountUpdate {
-    private String id;
+    @SerializedName("id")
+    private String userid;
+    @SerializedName("lang")
     private String lang;
+    @SerializedName("gender")
     private String gender;
 
-    public UserAccountUpdate(String id, String lang, String gender) {
-        this.id = id;
+    public UserAccountUpdate(String userid, String lang, String gender) {
+        this.userid = userid;
         this.lang = lang;
         this.gender = gender;
     }
 
-    // Getters and setters...
+    // Getters
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public String getLang() {
+        return lang;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    // Setters
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+
+    public void setLang(String lang) {
+        this.lang = lang;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 }
